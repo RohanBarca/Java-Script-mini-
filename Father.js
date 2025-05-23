de5cc58e9827875277d003965490bf77dac0d561
RohanBarca <rohanshuklatech629@gmail.com>
2025-05-23 12:57:40 +0530

    Create Father.js
    Father Detection Fun softwere

---
+++ Father.js
@@ -0,0 +1,12 @@
+let p=prompt("Enter the Name of the Child");
+function Child(){
+    let a= "Deepansh Shukla"
+    let b= "Rohan Shukla"
+    let c= "Binary"
+let d=Math.floor(Math.random()*3)
+if(d==0){return a}
+else if(d==1){return b}
+else if(d==2){return c}
+else if(d==3){return e}
+}
+console.log("The father of ", p ," is " , Child() )
